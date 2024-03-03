5d14c4872136a35a696c23b4b2300a14d0bbaaed
tanaka.satoshi <tanaka.satoshi@a-tm.co.jp>
2024-03-04 01:28:08 +0900

    add config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="h-screen">
+      <div className="max-w-4xl mx-auto py-20 px-6">
+        <h1 className={'text-4xl font-bold text-center text-zinc-200 mb-8'}>
+          React SOLID Todo List
+        </h1>
+        <hr />
+      </div>
+    </div>
+  )
+}
+
+export default App
